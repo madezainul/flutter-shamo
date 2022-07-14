a25be992598f7e5ec97651b87690dd183b975597
Made Zainul <madezainul@gmail.com>
2022-07-14 20:26:39 +0700

    Menambahkan dan finishing Product page

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/pages/product_page.dart';
 import 'package:shamo/theme.dart';
 
 class ProductCard extends StatelessWidget {
@@ -6,60 +7,65 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 215,
-      height: 278,
-      margin: EdgeInsets.only(
-        right: defaultMargin,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Color(0xffECEDEF),
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          Image.asset(
-            'assets/image_shoes.png',
-            width: 215,
-            height: 150,
-            fit: BoxFit.cover,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hiking',
-                style: secondaryTextStyle.copyWith(
-                  fontSize: 12,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/product');
+      },
+      child: Container(
+        width: 215,
+        height: 278,
+        margin: EdgeInsets.only(
+          right: defaultMargin,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Color(0xffECEDEF),
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Image.asset(
+              'assets/image_shoes.png',
+              width: 215,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hiking',
+                  style: secondaryTextStyle.copyWith(
+                    fontSize: 12,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 6,
-              ),
-              Text(
-                'COURT VISION 2.0',
-                style: blackTextStyle.copyWith(
-                  fontSize: 18,
-                  fontWeight: semiBold,
+                SizedBox(
+                  height: 6,
                 ),
-                overflow: TextOverflow.ellipsis,
-              ),
-              SizedBox(
-                height: 6,
-              ),
-              Text(
-                '\$58,67',
-                style: priceTextStyle.copyWith(
-                  fontSize: 14,
-                  fontWeight: medium,
+                Text(
+                  'COURT VISION 2.0',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: semiBold,
+                  ),
+                  overflow: TextOverflow.ellipsis,
                 ),
-              )
-            ],
-          ),
-        ],
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  '\$58,67',
+                  style: priceTextStyle.copyWith(
+                    fontSize: 14,
+                    fontWeight: medium,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
